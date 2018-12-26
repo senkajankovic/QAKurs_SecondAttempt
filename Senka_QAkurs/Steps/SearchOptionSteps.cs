69e78c20b3d66b966335c4fbb0b8abd84fb4b3e9
Senka Jankovic <senka.minic@gmail.com>
2018-12-26 12:34:37 +0100

    comments inserted

--- Senka_QAkurs/Steps/SearchOptionSteps.cs
+++ Senka_QAkurs/Steps/SearchOptionSteps.cs
@@ -27,19 +27,19 @@ namespace Senka_QAkurs.Steps
             ut.ClickOnElement(hp.SearchBtn);
         }
 
-        [Then(@"list of results including '(.*)' displays")]
+        [Then(@"list of results including '(.*)' displays")]                    //Proverava da li se kljucna rec nalazi u rezultatima search-a
         public void ThenListOfResultsIncludingKeywordDisplays(string keyword)
         {
             Utilities ut = new Utilities(Driver);
             SearchResultsListPage srlp = new SearchResultsListPage(Driver);
-            IList<IWebElement> results = Driver.FindElements(srlp.results);
-            int resultsCount = results.Count;
-            string title = null;
+            IList<IWebElement> results = Driver.FindElements(srlp.results);     //Kreiram listu IWebElemenata koja se zove reuslts
+            int resultsCount = results.Count;                                   //Proveravam koliko ima rezultata pretrage i dodeljujem tu vrednost integeru resultsCount
+            string title = "";                                                  //Inicijalizujem prazan string koji se zove title i koji cu smestati naslove elemenata
             for (int i = 0; i < resultsCount - 1; i++)
-            {
-                title = results[i].GetAttribute("title");
+            {                                                                   //Za svaki rezultat iz liste rezultata, vrednost atributa title smestam u string title
+                title = results[i].GetAttribute("title");                       //brojac i treba da bude za 1 manji od resultCount jer pocinje od 0
 
-                Assert.True(title.Contains(keyword));
+                Assert.True(title.Contains(keyword));                           //i proveravam da li sadrzi kljucnu rec zadatu u SpecFlow-u
             }
 
 
